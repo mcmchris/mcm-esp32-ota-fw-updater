92b1e39e3851249742d8a0536003c2c7a10cb01b
Christopher Méndez <49886387+mcmchris@users.noreply.github.com>
2025-11-20 09:31:00 -0400

    Library updated

--- MCM_GitHub_OTA.h
+++ MCM_GitHub_OTA.h
@@ -3,7 +3,6 @@
 
 #include <Arduino.h>
 #include <SPI.h>
-<<<<<<< HEAD
 #include <Ethernet.h>   
 #include <WiFi.h>
 #include <SSLClient.h>  
@@ -12,27 +11,12 @@
 
 #include "MCM_CertStore.h" 
 
-=======
-#include <Ethernet.h>   // Arduino Standard Ethernet Library
-#include <WiFi.h>
-#include <SSLClient.h>  // BearSSL (Software SSL)
-#include <ArduinoJson.h>
-#include <Update.h>
-
-#include "MCM_CertStore.h" // Tus certificados
-
-// Tipos de red interna
->>>>>>> 9dd72c7a9cd7fcd69037ca2a62c21df29e2eb557
 enum MCM_NetType {
     MCM_NET_NONE,
     MCM_NET_ETH,
     MCM_NET_WIFI
 };
 
-<<<<<<< HEAD
-=======
-// Estructura interna para headers
->>>>>>> 9dd72c7a9cd7fcd69037ca2a62c21df29e2eb557
 struct RespHdrBin {
     int status = 0;
     long long contentLen = 0;
@@ -43,7 +27,6 @@ struct RespHdrBin {
 
 class MCM_GitHub_OTA {
 public:
-<<<<<<< HEAD
     MCM_GitHub_OTA(bool enableEthernet, bool enableWiFi);
     ~MCM_GitHub_OTA(); // Destructor vacío ahora, pero lo dejamos por compatibilidad
 
@@ -51,25 +34,11 @@ public:
     void checkForUpdate();
 
 private:
-=======
-    // Constructor: Define qué interfaces quieres habilitar
-    MCM_GitHub_OTA(bool enableEthernet, bool enableWiFi);
-
-    // Configura los datos del repositorio
-    void begin(const char* owner, const char* repo, const char* currentVersion, const char* token = "");
-
-    // Llama a esto periódicamente para verificar actualizaciones
-    void checkForUpdate();
-
-private:
-    // Configuración
->>>>>>> 9dd72c7a9cd7fcd69037ca2a62c21df29e2eb557
     String _owner;
     String _repo;
     String _currentVersion;
     String _token;
 
-<<<<<<< HEAD
     bool _useEthernet;
     bool _useWiFi;
 
@@ -80,22 +49,6 @@ private:
     // YA NO GUARDAMOS LOS OBJETOS SSL AQUÍ PARA AHORRAR RAM
     
     // Helpers
-=======
-    // Flags de hardware
-    bool _useEthernet;
-    bool _useWiFi;
-
-    // Objetos de Red (Propios de la librería para gestionar SSL)
-    EthernetClient _eth_client;
-    WiFiClient     _wifi_client;
-
-    // Envoltorios SSL (BearSSL)
-    // Nota: Se inicializan en el constructor con el buffer de 18200 bytes
-    SSLClient _ssl_eth;
-    SSLClient _ssl_wifi;
-
-    // Helpers internos
->>>>>>> 9dd72c7a9cd7fcd69037ca2a62c21df29e2eb557
     String ua();
     String latestReleaseUrl();
     String hostOf(const String& url);
@@ -103,7 +56,6 @@ private:
     String pathOf(const String& url);
     
     MCM_NetType pickNetFast();
-<<<<<<< HEAD
     bool readLine(SSLClient* c, String& out, unsigned long timeoutMs = 45000);
     bool readHeadersBin(SSLClient* c, RespHdrBin& h);
     bool writeAllToUpdate(uint8_t* data, size_t len);
@@ -112,16 +64,6 @@ private:
     
     bool performUpdate(SSLClient* client, const String& startUrl, bool addAuth, const char* netName);
     bool getJson(SSLClient* client, const String& url, String& bodyOut, const char* netName);
-=======
-    bool readLine(SSLClient& c, String& out, unsigned long timeoutMs = 45000);
-    bool readHeadersBin(SSLClient& c, RespHdrBin& h);
-    bool writeAllToUpdate(uint8_t* data, size_t len);
-    bool pipeFixedToUpdate(SSLClient& c, long long contentLen, size_t displayTotal, size_t alreadyWrote, size_t& totalWrote);
-    bool pipeChunkedToUpdate(SSLClient& c);
-    
-    bool performUpdate(SSLClient& client, const String& startUrl, bool addAuth, const char* netName);
-    bool getJson(SSLClient& client, const String& url, String& bodyOut, const char* netName);
->>>>>>> 9dd72c7a9cd7fcd69037ca2a62c21df29e2eb557
     
     bool canFit(size_t content_len);
     void confirmOtaIfPending();
